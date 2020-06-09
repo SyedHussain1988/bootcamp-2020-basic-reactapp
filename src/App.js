ed179b0bc24d26f1acb8831ec5c2ba6f8101e8a2
SyedHussainAhmed121 <shas_virgo121@yahoo.com>
2020-06-09 22:17:56 +0500

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Child from './Child'
 
--- src/Child.js
+++ src/Child.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './Child.css'
 
 function Child(props) {
